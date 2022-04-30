7ffd3df64e6cf985eacb6063d02cfab5e20f1a70
Pedro Magalhães <pedromag1993@gmail.com>
2022-04-30 13:38:57 -0300

    Segunda parte: até inicialização do servidor Apache

--- configuracao_servidorWeb.sh
+++ configuracao_servidorWeb.sh
@@ -15,3 +15,24 @@ apt-get install -y wget
 
 #Passo 04 - Download do arquivo .zip
 wget https://gist.github.com/nunomazer/873219/archive/690e1f604dc8ead2583589e1aee6f8a42040a035.zip
+
+#Passo 05 - Instalação de pacote (zip e unzip)
+apt-get install -y zip unzip
+# Descompactando o arquivo zip
+unzip 690e1f604dc8ead2583589e1aee6f8a42040a035.zip
+# Entrando na pasta descompactada
+cd 873219-690e1f604dc8ead2583589e1aee6f8a42040a035 
+# Copiando os arquivos para outro diretório
+cp * /var/www/html/
+
+#Passo 06 - Acessando a pasta (/var/www/html/)
+cd /var/www/html/
+
+#Passo 07 - Removendo arquivo index.html da pasta
+rm index.html
+
+#Passo 08 - Renomeando arquivo (cv-resumido.html para index.html)
+mv cv-resumido.html index.html
+
+#Passo 09 - Iniciando servidor web Apache
+/etc/init.d/apache2 start
